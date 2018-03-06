c348d05c20962169fd516fbfd2f6a43ba0e7c70c
Zharphyn <brad@zharphyn.ca>
2018-03-06 20:21:10 +0000

    Added test file to test connection to database

---
+++ test_script.js
@@ -0,0 +1,30 @@
+"use strict";
+/*jslint node: true */
+/*jshint esversion: 6 */
+
+const pg = require("pg");
+// settings.json
+const settings = require("./settings");
+
+const client = new pg.Client({
+  user     : settings.user,
+  password : settings.password,
+  database : settings.database,
+  host     : settings.hostname,
+  port     : settings.port,
+  ssl      : settings.ssl
+});
+
+client.connect((err) => {
+  if (err) {
+    return console.error("Connection Error", err);
+  }
+  client.query("SELECT $1::int AS number", ["1"], (err, result) => {
+    if (err) {
+      return console.error("error running query", err);
+    }
+    console.log(result.rows[0].number); //output: 1
+    client.end();
+  });
+});
+
